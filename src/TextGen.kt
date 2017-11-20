b41f0c64061e05fb75eb50c9a405a93228ad8e0a
demoth <bubnov.d.e@gmail.com>
2017-11-20 23:32:51 +0300

    compound rules & categories

--- src/TextGen.kt
+++ src/TextGen.kt
@@ -14,22 +14,30 @@ fun main(args: Array<String>) {
     }
     val cmd = BasicParser().parse(o, args)
     val mapper = ObjectMapper(YAMLFactory())
-    val config = mapper.readValue(File(cmd.getOptionValue("c")), Map::class.java)
-    val iterations = cmd.getOptionValue("n").toInt()
-    val rand = Random(cmd.getOptionValue("s").toLong())
-    File(cmd.getOptionValue("o")).writer().use { w ->
+    val config = mapper.readValue(File(cmd.getOptionValue("c", "config.yaml")), Map::class.java)
+    val iterations = cmd.getOptionValue("n", "100").toInt()
+    val rand = Random(cmd.getOptionValue("s", Random().nextLong().toString()).toLong())
+    File(cmd.getOptionValue("o", "result.txt")).writer().use { w ->
         val categories = config["categories"] as Map<String, List<String>>
-        val rules = config["rules"] as List<List<String>>
+        val rules = config["rules"] as Map<String, List<String>>
         for (i in 0..iterations) {
-            val rule = rules.get(rand.nextInt(rules.size))
-            for (token in rule) {
-                val category = categories[token]
-                if (category != null)
-                    w.write(category[rand.nextInt(category.size)])
-                else
-                    w.write(token)
+            val rule = rules.entries.toList()[rand.nextInt(rules.size)]
+            for (token in rule.value) {
+                w.write(getWord(token, categories, rules, rand))
             }
         }
     }
 }
+
+
+fun getWord(token: String, categories: Map<String, List<String>>, rules: Map<String, List<String>>, r: Random): String {
+    val category = categories[token]
+    val rule = rules[token]
+    return when {
+        category != null -> getWord(category[r.nextInt(category.size)], categories, rules, r)
+        rule != null -> rule.map { getWord(it, categories, rules, r) }.joinToString(separator = "")
+        else -> token
+    }
+}
+
 
