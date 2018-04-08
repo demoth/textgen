13efe63489f00ec9bc3af38ae10f1dc563d772c9
demoth <bubnov.d.e@gmail.com>
2018-04-08 13:34:39 +0300

    show all categories

--- src/main/webapp/laoshi.js
+++ src/main/webapp/laoshi.js
@@ -1,14 +1,31 @@
 
+function onPageLoad () {
+    $.get( "/all_categories" + name, function (data, status) {
+            console.log("Data: " + data + "\nStatus: " + status);
+            var cat_list = document.getElementById("category_list");
+            var cats_obj = JSON.parse(data)
+            cat_list.innerHTML = createElementsForCategory(cats_obj);
+        }
+    );
+}
 
+function createElementsForCategory (cat_list_to_show) {
+    result = "";
+    console.log(cat_list_to_show)
+    Object.keys(cat_list_to_show).forEach(function(cat) {
+        result += "<li class='nav-item'> \
+        <a class='nav-link' id='jobs_category' onclick='loadCategory(\"" + cat_list_to_show[cat] + "\"); return false;' href='#'>\
+        <span data-feather='file'>\
+        </span> " + cat_list_to_show[cat]  + "</a></li>";
+    });
+    return result;
+}
 
 function loadCategory (name) {
-    $.ajax({
-      type: 'GET',
-      url: "/category/" + name,
-      success: function (data, status) {
-        document.getElementById("definition").value = data
-        document.getElementById("category_name").innerHTML  = name
-        console.log("Data: " + data + "\nStatus: " + status);}
-    });
-
+    $.get( "/category/" + name, function (data, status) {
+            document.getElementById("definition").value = data;
+            document.getElementById("category_name").innerHTML = name;
+            console.log("Data: " + data + "\nStatus: " + status);
+        }
+    );
 }
