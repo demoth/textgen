c13f4ca374893881b1727a6bda5642f8fb2f0a4a
demoth <bubnov.d.e@gmail.com>
2018-04-08 11:57:22 +0300

    alert on click

--- src/main/kotlin/laoshi/SampleController.kt
+++ src/main/kotlin/laoshi/SampleController.kt
@@ -1,12 +1,15 @@
 package laoshi
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SampleController {
-    @GetMapping("/hello")
-    fun hello(): String {
-        return "hell world"
+    @GetMapping("/category/{name}")
+    fun getCategoryBodyByName(@PathVariable name: String): String {
+        // TODO("Implement")
+        println(name)
+        return "$name:[我，你，他，她]"
     }
 }
